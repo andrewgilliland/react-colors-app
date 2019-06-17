2e2ce0fdd5c4bffab84cd834d7f65ec3a0d6fd99
andrewgilliland <39772195+andrewgilliland@users.noreply.github.com>
2019-06-17 12:36:57 -0500

    Add SingleColorPalette and Link

--- src/Palette.js
+++ src/Palette.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
 import ColorBox from "./ColorBox";
-import Navbar from './Navbar';
+import Navbar from "./Navbar";
 import "./Palette.css";
-
-
 
 class Palette extends Component {
   constructor(props) {
@@ -18,23 +16,33 @@ class Palette extends Component {
   }
 
   changeFormat(val) {
-      this.setState({ format: val });
+    this.setState({ format: val });
   }
 
   render() {
-    const { colors, paletteName, emoji } = this.props.palette;
+    const { colors, paletteName, emoji, id } = this.props.palette;
     const { level, format } = this.state;
     const colorBoxes = colors[level].map(color => (
-      <ColorBox background={color[format]} name={color.name} key={color.id}/>
+      <ColorBox
+        background={color[format]}
+        name={color.name}
+        key={color.id}
+        id={color.id}
+        paletteId={id}
+      />
     ));
 
     return (
       <div className="Palette">
-        <Navbar level={level} changeLevel={this.changeLevel} handleChange={this.changeFormat}/>
+        <Navbar
+          level={level}
+          changeLevel={this.changeLevel}
+          handleChange={this.changeFormat}
+        />
         <div className="Palette-colors">{colorBoxes}</div>
         <footer className="Palette-footer">
-            {paletteName}
-            <span className="emoji">{emoji}</span>
+          {paletteName}
+          <span className="emoji">{emoji}</span>
         </footer>
       </div>
     );
