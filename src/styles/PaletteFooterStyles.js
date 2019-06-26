7632659bbfbc8aaa34531e61a6beac3b9fdc93ef
andrewgilliland <39772195+andrewgilliland@users.noreply.github.com>
2019-06-26 12:15:48 -0500

    Move Out Styles

---
+++ src/styles/PaletteFooterStyles.js
@@ -0,0 +1,18 @@
+export default {
+    PaletteFooter: {
+      backgroundColor: "white",
+      height: "5vh",
+      fontWeight: "bold",
+    
+      display: "flex",
+      justifyContent: "flex-end",
+      alignItems: "center",
+    },
+    
+    emoji: {
+      fontSize: "1.5rem",
+      marginBottom: ".5rem",
+      marginLeft: "1rem",
+      marginRight: "1rem"
+    }
+  };
