67f7515314106128482dbd38a4bb6671a93e297e
andrewgilliland <39772195+andrewgilliland@users.noreply.github.com>
2019-07-10 11:59:11 -0500

    Tweak Form Styles

--- src/styles/PaletteFormNavStyles.js
+++ src/styles/PaletteFormNavStyles.js
@@ -6,6 +6,10 @@ const styles = theme => ({
   root: {
     display: "flex"
   },
+  
+  hide: {
+    display: "none"
+  },
 
   appBar: {
     transition: theme.transitions.create(["margin", "width"], {
