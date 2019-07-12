934e33dcdcecebbca5aecba8bd12f80180297c75
andrewgilliland <39772195+andrewgilliland@users.noreply.github.com>
2019-07-12 13:36:49 -0500

    Make ColorBox responsive

--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -16,12 +16,12 @@ export default {
 
       [sizes.down("lg")]: {
         width: "25%",
-        height: props => props.showingFullPalette ? "20%" : "50%"
+        height: props => props.showingFullPalette ? "20%" : "33.3333%"
       },
 
       [sizes.down("md")]: {
         width: "50%",
-        height: props => props.showingFullPalette ? "10%" : "50%"
+        height: props => props.showingFullPalette ? "10%" : "20%"
       },
 
       [sizes.down("xs")]: {
@@ -134,7 +134,11 @@ export default {
         width: "100%",
         textAlign: "center",
         marginBottom: "0",
-        padding: "1rem"
+        padding: "1rem",
+        textTransform: "uppercase",
+        [sizes.down("xs")]: {
+          fontSize: "6rem"
+        }
       },
       "& p": {
         fontSize: "2rem",
