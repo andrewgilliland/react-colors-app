ffb9696a72f11224037921ad01dd25f4246cbab3
Andrew Gilliland <andrewpgilliland@gmail.com>
2026-02-21 08:46:18 -0600

    chore: updates react

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(
-  React.createElement(
-    BrowserRouter,
-    null,
-    React.createElement(App, null)
-  ),
-  document.getElementById("root")
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  React.createElement(BrowserRouter, null, React.createElement(App, null))
 );
